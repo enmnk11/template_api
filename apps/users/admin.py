fd762f82e59515993bdf455f9755c18332d14129
enmnk11 <enmnk11@gmail.com>
2025-04-07 23:36:09 -0400

    is_active admin

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -5,7 +5,7 @@ from .models import User
 # Register your models here.
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
-    list_display = ('username', 'email', 'is_staff')
+    list_display = ('username', 'email', 'is_staff', 'is_active')
     search_fields = ('username', 'email')
     list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
 
